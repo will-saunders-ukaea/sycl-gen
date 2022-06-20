711701c836868e6f19d509a39ff8671cbb4cc5d9
Will Saunders <will.saunders@ukaea.uk>
2022-06-20 14:48:11 +0100

    basic malloc_shared on device

--- sycl_gen/main.py
+++ sycl_gen/main.py
@@ -1,5 +1,16 @@
 from sycl_gen import *
 
-syclcc = Compiler("syclcc", "-c --hipsycl-targets=omp -O3")
+# syclcc = Compiler("syclcc", "--hipsycl-targets=omp -O3 -fPIC -shared")
+syclcc = Compiler("syclcc", "--hipsycl-targets=cuda-nvcxx -O3 -fPIC -shared")
 
 sycl = SYCL(syclcc)
+
+N = 1024
+a_array = sycl.zeros_shared((N,), REAL)
+b_array = sycl.zeros_shared((N,), REAL)
+c_array = sycl.zeros_shared((N,), REAL)
+
+
+sycl.free(a_array)
+sycl.free(b_array)
+sycl.free(c_array)
--- sycl_gen/sycl_gen.py
+++ sycl_gen/sycl_gen.py
@@ -4,6 +4,8 @@ import numpy as np
 import shlex
 import subprocess
 import hashlib
+from functools import reduce
+from collections.abc import Iterable
 
 REAL = ctypes.c_double
 INT = ctypes.c_int64
@@ -29,23 +31,26 @@ class Compiler:
 
         with open(filename_header, "w") as fh:
             fh.write(header)
+            fh.write("\n")
 
         with open(filename_source, "w") as fh:
             fh.write('#include "' + h + '.hpp"\n')
             fh.write(source)
+            fh.write("\n")
 
-        cmd = (
-            [
-                self.binary,
-            ]
-            + [
-                filename_source,
-            ]
-            + self.args
-            + ["-o", filename_lib]
-        )
+        if not os.path.exists(filename_lib):
+            cmd = (
+                [
+                    self.binary,
+                ]
+                + [
+                    filename_source,
+                ]
+                + self.args
+                + ["-o", filename_lib]
+            )
 
-        subprocess.check_call(cmd)
+            subprocess.check_call(cmd)
 
         lib = ctypes.cdll.LoadLibrary(filename_lib)
 
@@ -57,8 +62,72 @@ class SYCL:
         self.compiler = compiler
 
         lib_header = """
+        #include <CL/sycl.hpp>
+        #include <iostream>
         """
         lib_src = """
+        
+        using namespace cl;
+        using namespace cl::sycl;
+
+        extern "C" int gen_initialise(sycl::device ** d, sycl::queue ** q){
+            *d = new sycl::device(sycl::default_selector());
+            *q = new sycl::queue(**d);
+            return 0;
+        }
+        extern "C" int gen_print_device(sycl::device *d, sycl::queue *q){
+            //std::cout << "Using " << d->get_info<sycl::info::device::name>()
+            //          << std::endl;
+            auto d2 = q->get_device();
+            std::cout << "Using " << d2.get_info<sycl::info::device::name>()
+                      << std::endl;
+            return 0;
+        }
+        extern "C" int gen_finalise(sycl::device ** d, sycl::queue ** q){
+            delete *q;
+            delete *d;
+            return 0;
+        }
+        extern "C" int gen_malloc_shared(size_t num_bytes, sycl::queue *q, char ** ptr){
+            *ptr = (char *) sycl::malloc_shared(num_bytes, *q);
+            return 0;
+        }
+        extern "C" int gen_sycl_free(sycl::queue *q, char *ptr){
+            sycl::free(ptr, *q);
+            return 0;
+        }
+
         """
 
         self.lib = compiler(lib_header, lib_src, "sycl_lib_")
+
+        self.device = ctypes.c_void_p(0)
+        self.queue = ctypes.c_void_p(0)
+
+        self.lib["gen_initialise"](ctypes.byref(self.device), ctypes.byref(self.queue))
+        self.lib["gen_print_device"](self.device, self.queue)
+
+    def __del__(self):
+
+        self.lib["gen_finalise"](ctypes.byref(self.device), ctypes.byref(self.queue))
+
+    def zeros_shared(self, shape, dtype):
+
+        if not issubclass(type(shape), Iterable):
+            shape = (shape,)
+        num_bytes = reduce(lambda x, y: x * y, shape) * ctypes.sizeof(dtype)
+
+        ptr = ctypes.c_void_p()
+
+        self.lib["gen_malloc_shared"](ctypes.c_size_t(num_bytes), self.queue, ctypes.byref(ptr))
+
+        typed_ptr = ctypes.cast(ptr, ctypes.POINTER(dtype))
+        array = np.ctypeslib.as_array(typed_ptr, shape=shape)
+
+        return array
+
+    def free(self, array):
+
+        ptr = ctypes.c_void_p(array.ctypes.data)
+
+        self.lib["gen_sycl_free"](self.queue, ptr)
