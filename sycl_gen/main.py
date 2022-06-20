58ba0701bace225f21bac5b317f719d150b56b81
Will Saunders <will.saunders@ukaea.uk>
2022-06-20 16:17:37 +0100

    proof of concept fma runs

--- sycl_gen/main.py
+++ sycl_gen/main.py
@@ -1,5 +1,8 @@
 from sycl_gen import *
 
+import numpy as np
+
+
 # syclcc = Compiler("syclcc", "--hipsycl-targets=omp -O3 -fPIC -shared")
 syclcc = Compiler("syclcc", "--hipsycl-targets=cuda-nvcxx -O3 -fPIC -shared")
 
@@ -9,8 +12,33 @@ N = 1024
 a_array = sycl.zeros_shared((N,), REAL)
 b_array = sycl.zeros_shared((N,), REAL)
 c_array = sycl.zeros_shared((N,), REAL)
+d_array = sycl.zeros_shared((N,), REAL)
 
+
+a_array[:] = np.random.uniform(size=(N,))
+b_array[:] = np.random.uniform(size=(N,))
+c_array[:] = np.random.uniform(size=(N,))
+
+correct = a_array * b_array + c_array
+
+
+a = KernelSymbol(a_array)
+b = KernelSymbol(b_array)
+c = KernelSymbol(c_array)
+d = KernelSymbol(d_array)
+ix = NDRange(N)
+
+
+fma = Executor(sycl, ix, Assign(d[ix], a[ix] * b[ix] + c[ix]))
+
+
+fma()
+
+err = np.linalg.norm(d_array - correct, np.inf)
+
+print("Infinity norm error:", err)
 
 sycl.free(a_array)
 sycl.free(b_array)
 sycl.free(c_array)
+sycl.free(d_array)
--- sycl_gen/sycl_gen.py
+++ sycl_gen/sycl_gen.py
@@ -6,6 +6,11 @@ import subprocess
 import hashlib
 from functools import reduce
 from collections.abc import Iterable
+from collections import OrderedDict
+
+import pymbolic as pmbl
+from pymbolic.mapper.c_code import CCodeMapper as CCM
+
 
 REAL = ctypes.c_double
 INT = ctypes.c_int64
@@ -131,3 +136,108 @@ class SYCL:
         ptr = ctypes.c_void_p(array.ctypes.data)
 
         self.lib["gen_sycl_free"](self.queue, ptr)
+
+
+class KernelSymbol(pmbl.primitives.Variable):
+
+    name_counter = 0
+    name_base = "sym_"
+
+    def __init__(self, obj, name=None):
+        if name is None:
+            name = KernelSymbol.name_base + str(KernelSymbol.name_counter)
+            KernelSymbol.name_counter += 1
+        super().__init__(name)
+        self.gen_obj = obj
+
+
+class NDRange(pmbl.primitives.Variable):
+    def __init__(self, n, name="idx"):
+        super().__init__(name)
+        self.gen_n = n
+
+
+class Assign:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+        self.op = "="
+
+
+class Executor:
+    def __init__(self, sycl, iteration_set, *exprs, **kwargs):
+
+        self.sycl = sycl
+
+        lib_header = """
+        #include <CL/sycl.hpp>
+        #include <iostream>
+        #include <cstdint>
+        """
+
+        dependency_mapper = pmbl.mapper.dependency.DependencyMapper(
+            include_subscripts=False,
+            include_lookups=False,
+            include_calls=False,
+            include_cses=False,
+            composite_leaves=None,
+        )
+
+        self.parameters = OrderedDict()
+        expressions = []
+
+        def process_dependency(dep):
+            if issubclass(type(dep), KernelSymbol):
+                self.parameters[dep.name] = dep.gen_obj
+
+        ccm = CCM()
+        for expr in exprs:
+            expressions.append(ccm(expr.lhs) + " " + expr.op + " " + ccm(expr.rhs) + ";")
+
+            for dx in dependency_mapper(expr.lhs):
+                process_dependency(dx)
+            for dx in dependency_mapper(expr.rhs):
+                process_dependency(dx)
+
+        lib_src = """
+        using namespace cl;
+        using namespace cl::sycl;
+        extern "C" int wrapper(
+            sycl::queue *queue,
+            {PARAMETERS}
+        ){{
+              queue->submit([&](sycl::handler &cgh) {{
+               cgh.parallel_for<>(sycl::range<1>({LOOP_EXTENT}), [=](sycl::id<1> {LOOP_INDEX}) {{
+                {EXPRESSIONS}
+              }});
+            }}).wait();
+
+
+            return 0;
+        }}
+
+        """.format(
+            LOOP_EXTENT=str(iteration_set.gen_n),
+            LOOP_INDEX=iteration_set.name,
+            EXPRESSIONS="\n".join(expressions),
+            PARAMETERS=",\n".join([self.format_parameters(fx) for fx in self.parameters.items()]),
+        )
+
+        self.lib = sycl.compiler(lib_header, lib_src)["wrapper"]
+
+    def format_parameters(self, fx):
+        ctype_map = {REAL: "double", INT: "int64_t"}
+
+        param = "{CTYPE} * {NAME}".format(CTYPE=ctype_map[np.ctypeslib.as_ctypes_type(fx[1].dtype)], NAME=fx[0])
+
+        return param
+
+    def get_argument(self, fx):
+        return fx[1].ctypes.get_as_parameter()
+
+    def __call__(self):
+        args = [
+            self.sycl.queue,
+        ]
+        args += [self.get_argument(fx) for fx in self.parameters.items()]
+        self.lib(*args)
